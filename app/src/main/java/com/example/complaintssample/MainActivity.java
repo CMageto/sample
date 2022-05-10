d1e5732cbc089acd400b8d09771770df96c7c1f9
Sharon Ashuka <68126270+anony45@users.noreply.github.com>
2022-05-10 21:02:10 +0300

    Initial commit

---
+++ app/src/main/java/com/example/complaintssample/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.complaintssample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+	Button deposit,withdraw,bets;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		deposit=findViewById(R.id.deposit);
+		withdraw=findViewById(R.id.withdraw);
+		bets=findViewById(R.id.bets);
+
+
+		deposit.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Intent intent = new Intent(this,Deposit.class);
+			}
+		});
+		withdraw.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Intent intent = new Intent(this,withdraw.class);
+			}
+		});
+		bets.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Intent intent = new Intent(this,bets.class);
+			}
+		});
+
+
+	}
+}
